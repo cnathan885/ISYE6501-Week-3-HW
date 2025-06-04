7ded69e126a9863aa4228b56d2439226b42d3dfe
Nathan Chiang <nachiang@stanford.edu>
2025-06-04 13:53:29 -0700

    yee

---
+++ Week3HWCode.R
@@ -0,0 +1,11 @@
+#------Question 7.2------#
+
+install.packages("tidyverse")
+install.packages("fpp2")
+library(tidyverse)
+library(fpp2)
+
+data <- read.table("temps.txt", header=TRUE)
+
+head (data)
+data
